750610dca1f7b2bd84c47e52b304bcbae92131f6
cbarrios <lalobarrios16@yahoo.com>
2023-01-09 11:51:13 -0500

    master: (1) clean template

---
+++ app/src/main/java/com/lalosapps/alarmmanager/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.lalosapps.alarmmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.lalosapps.alarmmanager.ui.theme.AlarmManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AlarmManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
